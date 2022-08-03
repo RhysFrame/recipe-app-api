e38cf166c7abbedc38f1547eb1c94912e8ff4a33
rhysframe <rhysframe@gmail.com>
2022-08-04 08:48:27 +1000

    Update tests and docker-compose.yml

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,7 +2,6 @@
 Sample tests
 """
 
-import imp
 from django.test import SimpleTestCase
 
 from app import calc 
@@ -16,3 +15,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5,6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res, 5)
