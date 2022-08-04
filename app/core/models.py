63977acd22f91f43518d871d15cedb16b5e6f6ee
rhysframe <rhysframe@gmail.com>
2022-08-04 14:45:53 +1000

    Create model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
+"""
+Database models.
+"""
+from pyexpat import model
+from tkinter.tix import Tree
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
