651de263e834451c1c3c4e5e608bc76822de3b52
rhysframe <rhysframe@gmail.com>
2022-09-08 10:06:18 +1000

    Allow for database resets

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
